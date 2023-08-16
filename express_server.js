aebb022576d56c412e8e4ec25e7bfa43e38fd36b
Kenny Lee <kenny.lee927@gmail.com>
2023-08-16 18:45:25 -0400

    add route to urls/:id template page

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -25,6 +25,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: `${urlDatabase[req.params.id]}` /* What goes here? */,
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-}); 
+});
