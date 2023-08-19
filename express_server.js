0410d93e4d4adf60a331575bc4b881dd66994b70
Kenny Lee <kenny.lee927@gmail.com>
2023-08-18 22:25:51 -0400

    include cookieParser and send username cookie to all pages

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,16 +24,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     id: req.params.id,
     longURL: `${urlDatabase[req.params.id]}` /* What goes here? */,
   };
@@ -72,9 +76,9 @@ app.get("/u/:id", (req, res) => {
 // navbar login form
 app.post("/login", (req, res) => {
   inputtedUsername = req.body.username;
-  res.cookie('username', inputtedUsername);
+  res.cookie("username", inputtedUsername);
   res.redirect(`/urls/`);
-})
+});
 
 // allows users to connect
 app.listen(PORT, () => {
