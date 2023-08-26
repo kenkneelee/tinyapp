2fe096f9f8412954bcc27893f930cd88ce7d5f8c
Kenny Lee <kenny.lee927@gmail.com>
2023-08-26 06:19:50 -0400

    add 404 for nonexistent urls

--- express_server.js
+++ express_server.js
@@ -49,8 +49,12 @@ app.get("/urls/:id", (req, res) => {
 
 // redirect to associated longurl
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(`${longURL}`);
+  if (!urlDatabase[req.params.id]) {
+    res.status(404).send("Link does not exist");
+  } else {
+    const longURL = urlDatabase[req.params.id];
+    res.redirect(`${longURL}`);
+  }
 });
 
 // add new url entry
